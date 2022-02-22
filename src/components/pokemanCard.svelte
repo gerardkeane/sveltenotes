e9b527de295f78af55126e30949abc8a0d4c4528
Gerard Keane <g.keane@me.com>
2022-02-22 21:45:03 +0000

    Altered card styling

--- src/components/pokemanCard.svelte
+++ src/components/pokemanCard.svelte
@@ -3,8 +3,8 @@
     export let pokeman;
 </script>
 
-<a sveltekit:prefetch class="p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-lg flex flex-col items-center"
+<a sveltekit:prefetch class="p-6 bg-yellow-300 text-black text-center rounded-md shadow-sm hover:shadow-lg flex flex-col items-center"
     href={`/pokemon/${pokeman.id}`} transition:fade>
-    <img class="w-40 h-40" src={pokeman.image} alt={pokeman.name}>
+    <img class="w-44 h-44" src={pokeman.image} alt={pokeman.name}>
     <h2 class="uppercase text-xl font-bold">{pokeman.name}</h2>
 </a>
