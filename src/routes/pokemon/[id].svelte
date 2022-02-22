280e48f9557103931477d478355cec6b584b3476
Gerard Keane <g.keane@me.com>
2022-02-22 21:45:20 +0000

    Added more pokemon information

--- src/routes/pokemon/[id].svelte
+++ src/routes/pokemon/[id].svelte
@@ -3,18 +3,28 @@
         const id = params.id;
         const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
         const res = await fetch(url)
-        const pokeman = await res.json()
-        return {props: {pokeman}}
+        const data = await res.json()
+        return {props: {data}}
     }
 </script>
 
 <script>
-    export let pokeman;
-    const type = pokeman.types[0].type.name
+    export let data;
+    const type = data.types.map(type => type.type.name).join(", ")
+    const abilities = data.abilities.map(ability => ability.ability.name).join(', ')
+    const heldItems = data.held_items.map(item => item.item.name).join(", ")
 </script>
 
 <div class="flex flex-col items-center">
-    <h1 class="text-4xl font-bold uppercase my-8 text-center">{pokeman.name} </h1>
-    <p>Type: <strong>{type}</strong> | Height: <strong>{pokeman.height}</strong> | Weight: <strong>{pokeman.weight}</strong></p>
-    <img class="card-image" src={pokeman.sprites['front_default']} alt={pokeman.name} />
+    <h1 class="text-4xl font-bold uppercase my-8 text-center">{data.name} </h1>
+    <img class="card-image w-96 h-96" src={data.sprites['front_default']} alt={data.name} />
+    <p>Type: <strong>{type}</strong> | Height: <strong>{data.height}</strong> | Weight: <strong>{data.weight}</strong></p>
+    <p>Abilities: <strong>{abilities}</strong></p>
+    <p>Held Items:
+        {#if heldItems.length}
+        <strong>{heldItems}</strong>
+        {:else}
+        <strong>None</strong>
+        {/if}
+    </p>
 </div>
