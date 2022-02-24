3477201d8192d9cd73cf1a1556c0d78353ceeb32
Gerard Keane <g.keane@me.com>
2022-02-24 12:46:20 +0000

    returned to gray bg

--- src/components/pokemanCard.svelte
+++ src/components/pokemanCard.svelte
@@ -3,7 +3,7 @@
     export let pokeman;
 </script>
 
-<a sveltekit:prefetch class="p-6 bg-yellow-300 text-black text-center rounded-md shadow-sm hover:shadow-lg flex flex-col items-center"
+<a sveltekit:prefetch class="p-6 bg-gray-200 text-black text-center rounded-md shadow-sm hover:shadow-lg flex flex-col items-center"
     href={`/pokemon/${pokeman.id}`} transition:fade>
     <img class="w-44 h-44" src={pokeman.image} alt={pokeman.name}>
     <h2 class="uppercase text-xl font-bold">{pokeman.name}</h2>
